503ab17cc7df32a1bc594c436a00f00421906950
Poojith L <lpoojith9@gmail.com>
2025-07-08 17:37:47 +0530

    Time: 896 ms (5.14%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+    select max(e1.salary) as SecondHighestSalary from Employee e1 where(select      count(distinct e2.salary) from Employee e2 where e2.salary>e1.salary)=N-1
+  );
+END
